be575cedbe14f4b66b7c98cddcc8d30d25c01e50
Brandon Copeland <copelandbrandon2@gmail.com>
2021-06-21 21:29:56 +0000

    implement solution for exercise_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.find_by(id: 2)
+
+puts @store1.name, @store2.name
+
+@store1.name = "Not Surrey Anymore"
+
+puts @store1.name
