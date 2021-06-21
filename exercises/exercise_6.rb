f27cd14ada0f9696a1a01b7724328f49f1986af1
Brandon Copeland <copelandbrandon2@gmail.com>
2021-06-21 22:29:39 +0000

    implement solution for exercise_6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+@store1.employees.create(first_name: "Jim", last_name: "Henson", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Edward", last_name: "Scissorhands", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Jane", last_name: "Doe", hourly_rate: 60)
+
